c42ec7cd7f92b5be0ef84ff70d4fdd6426e6dbb2
nathanjaekim25-commits <nathanjaekim.25@gmail.com>
2025-11-09 00:13:31 -0500

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab03")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team 114, Web Development - Section B")
+st.subheader("Shreyas Griota, Nathan Kim")
+st.write("Our project will use the NASA Techport API that lists their programs, current projects and technologies! The API reqeuest URL is: https://techport.nasa.gov/help/api")
+
+st.divider()
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. **API Graphs**: Page that illustrates API information in an interactible format
+2. **None for now**
+3.
+4.
+
+""")
+
+
